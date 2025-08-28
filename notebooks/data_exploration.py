cce8c1981fbf62383faf7d023f9124acd14b40ba
Leonardo Meduna <leonardo.meduna231@gmail.com>
2025-08-28 11:16:26 -0300

    Feat: Plotando gráficos para o relatório de análise

--- notebooks/data_exploration.py
+++ notebooks/data_exploration.py
@@ -94,3 +94,109 @@ plt.xticks(ticks=[0, 1], labels=['Bis', 'Hershey\'s (Mais)'], rotation=0, fontsi
 plt.legend(title='Condição', labels=['Sem Marca', 'Com Marca'])
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.show()
+
+# %%
+# Agrupando o dataframe original por Condição para conseguir trazer um resumo geral
+df_genero_agg = df[filtro_s_marca].groupby(by="Gênero")[['Cor_Bis',
+                           'Cor_Mais',	
+                           'Aroma_Bis',	
+                           'Aroma_Mais',
+                            'Sabor_Bis',
+                            'Sabor_Mais',	
+                            'Crocância_Bis',	
+                            'Crocância_Mais',
+                            'Consitência_Bis',
+                            'Consistência_Mais',
+                            'Residual_Bis',	
+                            'Residual_Mais',
+                            'Avaliacao_geral_Bis',	
+                            'Avaliacao_geral_Mais',
+                            'Compraria_Bis',
+                            'Compraria_Mais',	
+                            'Condição']].mean()
+df_genero_agg = pd.DataFrame(df_genero_agg, index=[0, 1])
+df_genero_agg
+
+# %%
+plt.figure(figsize=(10, 6))
+df_genero_agg[['Sabor_Bis', 'Sabor_Mais']].T.plot(kind='bar', figsize=(10, 6), color=['#A52A2A', '#8B4513'])
+plt.title('Média de Sabor por Genêro')
+plt.xlabel('Marca')
+plt.ylabel('Nota Média')
+plt.xticks(ticks=[0, 1], labels=['Bis', 'Hershey\'s (Mais)'], rotation=0, fontsize='large')
+plt.legend(title='Genêro', labels=['Masculino', 'Feminino'])
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+# %%
+plt.figure(figsize=(10, 6))
+df_genero_agg[['Avaliacao_geral_Bis', 'Avaliacao_geral_Mais']].T.plot(kind='bar', figsize=(10, 6), color=['#A52A2A', '#8B4513'])
+plt.title('Média de Avaliação por Genêro')
+plt.xlabel('Marca')
+plt.ylabel('Nota Média')
+plt.xticks(ticks=[0, 1], labels=['Bis', 'Hershey\'s (Mais)'], rotation=0, fontsize='large')
+plt.legend(title='Genêro', labels=['Masculino', 'Feminino'])
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+# %%
+plt.figure(figsize=(10, 6))
+df_genero_agg[['Compraria_Bis', 'Compraria_Mais']].T.plot(kind='bar', figsize=(10, 6), color=['#A52A2A', '#8B4513'])
+plt.title('Média de Inteção de Compra por Genêro')
+plt.xlabel('Marca')
+plt.ylabel('Nota Média')
+plt.xticks(ticks=[0, 1], labels=['Bis', 'Hershey\'s (Mais)'], rotation=0, fontsize='large')
+plt.legend(title='Genêro', labels=['Masculino', 'Feminino'])
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+# %%
+df_genero_agg_1 = df[filtro_c_marca].groupby(by="Gênero")[['Cor_Bis',
+                           'Cor_Mais',	
+                           'Aroma_Bis',	
+                           'Aroma_Mais',
+                            'Sabor_Bis',
+                            'Sabor_Mais',	
+                            'Crocância_Bis',	
+                            'Crocância_Mais',
+                            'Consitência_Bis',
+                            'Consistência_Mais',
+                            'Residual_Bis',	
+                            'Residual_Mais',
+                            'Avaliacao_geral_Bis',	
+                            'Avaliacao_geral_Mais',
+                            'Compraria_Bis',
+                            'Compraria_Mais',	
+                            'Condição']].mean()
+df_genero_agg_1 = pd.DataFrame(df_genero_agg_1, index=[0, 1])
+df_genero_agg_1
+
+# %%
+plt.figure(figsize=(10, 6))
+df_genero_agg_1[['Sabor_Bis', 'Sabor_Mais']].T.plot(kind='bar', figsize=(10, 6), color=['#A52A2A', '#8B4513'])
+plt.title('Média de Sabor por Genêro')
+plt.xlabel('Marca')
+plt.ylabel('Nota Média')
+plt.xticks(ticks=[0, 1], labels=['Bis', 'Hershey\'s (Mais)'], rotation=0, fontsize='large')
+plt.legend(title='Genêro', labels=['Masculino', 'Feminino'])
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+# %%
+plt.figure(figsize=(10, 6))
+df_genero_agg_1[['Avaliacao_geral_Bis', 'Avaliacao_geral_Mais']].T.plot(kind='bar', figsize=(10, 6), color=['#A52A2A', '#8B4513'])
+plt.title('Média de Avaliação por Genêro')
+plt.xlabel('Marca')
+plt.ylabel('Nota Média')
+plt.xticks(ticks=[0, 1], labels=['Bis', 'Hershey\'s (Mais)'], rotation=0, fontsize='large')
+plt.legend(title='Genêro', labels=['Masculino', 'Feminino'])
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+# %%
+plt.figure(figsize=(10, 6))
+df_genero_agg_1[['Compraria_Bis', 'Compraria_Mais']].T.plot(kind='bar', figsize=(10, 6), color=['#A52A2A', '#8B4513'])
+plt.title('Média de Inteção de Compra por Genêro')
+plt.xlabel('Marca')
+plt.ylabel('Nota Média')
+plt.xticks(ticks=[0, 1], labels=['Bis', 'Hershey\'s (Mais)'], rotation=0, fontsize='large')
+plt.legend(title='Genêro', labels=['Masculino', 'Feminino'])
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
